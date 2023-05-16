a950611f6547c087653ba46f6c815714d5ee53e7
Jakub Fryč <kubacz@centrum.cz>
2023-05-17 00:00:39 +0200

    Přidáno odstraňování kalendářových eventů, přidány komentáře

--- Kalendar/Program.cs
+++ Kalendar/Program.cs
@@ -3,6 +3,7 @@ using Spectre.Console;
 
 // Tato proměnná je určena k tomu, aby byl program schopný říct, zda byla/nebyla nalezena událost ke smazání/prohlédnutí.
 bool udalostNalezena = false;
+// Tato proměnná zajišťuje opakovaný chod cyklu celého programu
 bool wantsToRun = true;
 DateOnly soucasneDatum = DateOnly.FromDateTime(DateTime.Now);
 List<Udalost> listUdalosti = new List<Udalost>();
@@ -22,13 +23,18 @@ while (wantsToRun)
             .AddChoices(new[] {
             "Kalendář aktuálního měsíce", "Přidat událost", "Smazat událost", "Zobrazit události", "Ukončit program",
             }));
+            // Program nyní rozhoduje co uživatel vybral a na základě toho operuje.
         switch (akce)
         {
+            // Vypsat kalendář
             case "Kalendář aktuálního měsíce":
+                Console.Clear();
                 AnsiConsole.Write(kalendář);
                 break;
+            // Přidat událost
             case "Přidat událost":
                 Console.Clear();
+                // Program vyžaduje po uživateli datum, do kterého chce zapsat událost.
                 var rokUdalosti = AnsiConsole.Ask<int>("Zadej rok. (YYYY)");
                 var mesicUdalosti = AnsiConsole.Ask<int>("Zadej měsíc. (MM)");
                 var denUdalosti = AnsiConsole.Ask<int>("Zadej den. (DD)");
@@ -36,33 +42,39 @@ while (wantsToRun)
                 DateOnly datumUdalosti;
                 try
                 {
+                    // Program nyní dosadí uživatelem daný datum do objektu datumUdalosti
                     datumUdalosti = new DateOnly(rokUdalosti, mesicUdalosti, denUdalosti);
                     kalendář.AddCalendarEvent(rokUdalosti, mesicUdalosti, denUdalosti);
                 }
                 catch (Exception e)
                 {
+                    // Ošetření výjimky -> Spadne na začátek programu
                     AnsiConsole.MarkupLine($"Nastala chyba! Jsi si jistý, že jsi zadal rok, měsíc či den ve správném formátu?");
                     AnsiConsole.MarkupLine(e.Message);
                     break;
                 }
 
                 var nadpisUdalosti = AnsiConsole.Ask<string>("Zadej nadpis.");
-                var obsahUdalosti = AnsiConsole.Ask<string>("(Volitelné) Zadej podrobný popis události.");
+                var obsahUdalosti = AnsiConsole.Ask<string>("Zadej podrobný popis události.");
 
+                // Zde se vytvoří objekt udalost, do kterého se dosadí parametry, které jsme vytvořili předtím.
                 Udalost udalost = new Udalost(datumUdalosti, nadpisUdalosti, obsahUdalosti);
+                // Nakonec se přidá událost do seznamu
                 listUdalosti.Add(udalost);
                 Console.Clear();
                 AnsiConsole.MarkupLine("Událost byla úspěšně přidána!");
                 break;
+                // Smazat událost
             case "Smazat událost":
                 Console.Clear();
+                // Program zkontroluje, jestli není list událostí prázdný. Pokud ano, spadne na začátek.
                 if (listUdalosti.Count == 0)
                 {
                     Console.Clear();
                     AnsiConsole.MarkupLine("[red]V kalendáři nejsou žádné události![/]");
                     break;
                 }
-
+                // Aby uživatel věděl, které události má k dispozici a jaké mají ID, program mu to vypíše.
                 foreach (var item in listUdalosti)
                 {
 
@@ -76,34 +88,38 @@ while (wantsToRun)
                 }
                 else
                 {
+                    // Program projíždí všechny události z listu.
                     foreach (var item in listUdalosti)
                     {
+                        // Pokud najde shodu v ID události a ID zadaným uživatelem, stane se následující:
                         if (item.IdUdalosti == smazanaUdalost)
                         {
                             Console.Clear();
+                            // Tato proměnná se nastaví, aby program poté mohl zkontrolovat, jestli proběhlo mazání v pořádku.
                             udalostNalezena = true;
                             var tempTask = item;
-                            Udalost.Counter--;
+                            // Jelikož jsem nenašel metodu, která by ve SpectreConsole mazala kalendářovou událost, musím to udělat ručně :)))
+                            // Program projede všechny datumy v kalendáři.
                             foreach (var eventy in kalendář.CalendarEvents)
                             {
-                                if (eventy.Year == tempTask.DatumUdalosti.Year && eventy.Month == tempTask.DatumUdalosti.Month && eventy.Month == tempTask.DatumUdalosti.Day)
+                                // Do tohoto objektu si dosadíme kvůli formátování ve třídě DateOnly Rok, Měsíc a Den z objektu kalendáře.
+                                DateOnly dateTempTask = new DateOnly(eventy.Year, eventy.Month, eventy.Day);
+
+                                // Program hledá shodu v Roku, Měsíci a Dni
+                                if (dateTempTask.Year == tempTask.DatumUdalosti.Year && dateTempTask.Month == tempTask.DatumUdalosti.Month && dateTempTask.Day == tempTask.DatumUdalosti.Day)
                                 {
-                                    Console.WriteLine(item);
-                                    Console.WriteLine(eventy);
+                                    // Program maže událost z kalendáře
                                     kalendář.CalendarEvents.Remove(eventy);
+                                    // Program maže událost z listu událostí
                                     listUdalosti.Remove(item);
-                                    Console.ReadKey();
-                                }
-                                else
-                                {
-                                    Console.WriteLine($"test");
-                                    Console.ReadKey();
+                                    break;
                                 }
                             }
                             
                             break;
                         }
                     }
+                    
                     if (udalostNalezena != true)
                     {
                         AnsiConsole.MarkupLine("[red]Událost s tímto ID neexistuje![/]");
@@ -113,14 +129,17 @@ while (wantsToRun)
 
 
                 break;
+                // Zobrazení události
             case "Zobrazit události":
+                Console.Clear();
+                // Program zkontroluje, jestli není list událostí prázdný. Pokud ano, spadne na začátek.
                 if (listUdalosti.Count == 0)
                 {
                     Console.Clear();
                     AnsiConsole.MarkupLine("[red]V kalendáři nejsou žádné události![/]");
                     break;
                 }
-
+                // Aby uživatel věděl, které události má k dispozici a jaké mají ID, program mu to vypíše.
                 foreach (var item in listUdalosti)
                 {
 
@@ -143,12 +162,16 @@ while (wantsToRun)
                 }
                 udalostNalezena = false;
                 break;
+                // Ukočit program
             case "Ukončit program":
+                Console.Clear();
+                // Vypisování obrázku
                 var image = new CanvasImage("logoo.png");
                 image.MaxWidth(18);
                 Console.Clear();
                 AnsiConsole.MarkupLine("[green]Melon Taskmin[/] byl úspěšně ukončen!");
                 AnsiConsole.Write(image);
+                // Ukončení cyklu pro běh programu
                 wantsToRun = false;
                 Console.ReadKey();
                 break;
